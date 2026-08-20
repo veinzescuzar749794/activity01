75cf0f57f88f05f9e692da4ce1f811529bea1ce8
Activity 01 Student <student@example.com>
2026-08-20 18:59:51 +0800

    chore: add Spring Boot backend baseline

---
+++ src/main/java/edu/cit/escuzar/activity01/Activity01Application.java
@@ -0,0 +1,13 @@
+package edu.cit.escuzar.activity01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activity01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Activity01Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cit/escuzar/activity01/Activity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.escuzar.activity01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
